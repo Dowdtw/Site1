2a4c7f926a1f3ea169e7729b6b783cec286e0b59
Dowdtw <47797395+Dowdtw@users.noreply.github.com>
2020-01-26 21:30:05 +0000

    Update index1.js

--- index1.js
+++ index1.js
@@ -1,6 +1,6 @@
 const queryString = window.location.search
 const urlParams = new URLSearchParams(queryString)
-const clickId = urlParams.get('clickid')
+const clickId = urlParams.get('ClickId')
 
 function set_cookie(name, value){
 		const cookie = [name, '=', JSON.stringify(value), '; domain=.', window.location.host.toString(), '; path=/;'].join('');
