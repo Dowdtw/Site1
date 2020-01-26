47e84e225fb42809ba76c4a36ca9bc9fdfec347f
Dowdtw <47797395+Dowdtw@users.noreply.github.com>
2020-01-26 16:24:31 +0000

    Update index1.js

--- index1.js
+++ index1.js
@@ -6,10 +6,8 @@ window.onload = function() {
 				console.log(clickid)
 				} catch (err) {
 				console.log('Issues with Parsing URL Params' + err);
+				document.cookie = "irClickId = clickid"
 			}
-	document.cookie = "irClickId = clickid"
-	
-	
 }
 	
 	
