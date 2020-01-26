f2005327d50cb23039c511d71baaa1f3d91d9c74
Dowdtw <47797395+Dowdtw@users.noreply.github.com>
2020-01-26 21:23:39 +0000

    Update index1.js

--- index1.js
+++ index1.js
@@ -1,22 +1,7 @@
-/*const myClickid = window.onload = function() {
-			try {
-				var url_string = (window.location.href).toLowerCase();
-				var url = new URL(url_string);
-       				const clickid = url.searchParams.get('clickid');
-				return clickid;
-				} catch (err) {
-				console.log('Issues with Parsing URL Params' + err);
-				
-				}
-
-}*/
 const queryString = window.location.search
 const urlParams = new URLSearchParams(queryString)
 const clickId = urlParams.get('clickid')
 
-
-
-	
 function set_cookie(name, value){
 		const cookie = [name, '=', JSON.stringify(value), '; domain=.', window.location.host.toString(), '; path=/;'].join('');
 		document.cookie = cookie;
