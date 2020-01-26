8e296a77915d1002154de509ebbb5d8b918e1858
Dowdtw <47797395+Dowdtw@users.noreply.github.com>
2020-01-26 15:02:34 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+<script>
+window.onload = function(){
+	try {
+		const url_string = (window.location.href).toLowerCase();
+		const url = new URL(url_string);
+		const clickid = url.searchParams.get('clickid');
+		console.log(clickid)
+	} catch (err){
+		console.log('issues with parsing url param' + err);
+	}
+}
+
+
+
+
+
+
+
+</script>
